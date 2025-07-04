186b7d08f3918ee78786b3c482de129c81f025ab
Adelodunpeter25 <adelodunpeter69@gmail.com>
2025-07-04 15:56:26 +0100

    worked on the frontend

--- taskmanager/accounts/views.py
+++ taskmanager/accounts/views.py
@@ -1,11 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib import messages
 
 def login_view(request):
-    return render(request, 'accounts/login.html')
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.success(request, f'Welcome back, {username}!')
+                return redirect('home')
+        else:
+            messages.error(request, 'Invalid username or password')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'accounts/login.html', {'form': form})
 
 def signup_view(request):
-    return render(request, 'accounts/signup.html')
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, f'Account created successfully! Welcome, {user.username}!')
+            return redirect('home')
+        else:
+            messages.error(request, 'Error creating account. Please check the form.')
+    else:
+        form = UserCreationForm()
+    return render(request, 'accounts/signup.html', {'form': form})
 
 def logout_view(request):
-    # We'll implement logout logic later
-    return render(request, 'accounts/logout.html')
+    logout(request)
+    messages.info(request, 'You have been logged out successfully.')
+    return redirect('home')
--- taskmanager/taskmanager/settings.py
+++ taskmanager/taskmanager/settings.py
@@ -31,6 +31,16 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
+# Message tags for Bootstrap alerts
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-info',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+}
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -117,9 +127,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
-
-STATIC_URL = 'static/'
 TEMPLATES[0]['DIRS'] = [BASE_DIR / 'templates']
+STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
 
@@ -127,3 +137,8 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Authentication settings
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
+LOGIN_URL = 'login'
