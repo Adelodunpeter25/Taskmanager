790ecc3cb82567cb02f956f2da0b0c806f1da1a7
Adelodunpeter25 <adelodunpeter69@gmail.com>
2025-07-04 15:14:24 +0100

    first commit

--- taskmanager/accounts/views.py
+++ taskmanager/accounts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+def login_view(request):
+    return render(request, 'accounts/login.html')
+
+def signup_view(request):
+    return render(request, 'accounts/signup.html')
+
+def logout_view(request):
+    # We'll implement logout logic later
+    return render(request, 'accounts/logout.html')
--- taskmanager/projects/views.py
+++ taskmanager/projects/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
 
-# Create your views here.
+def project_home(request):
+    return render(request, 'projects/home.html')  # We'll create this template shortly
